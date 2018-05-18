80a88d1bb8a6746d7883ed37960ab681b0882325
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-18 15:00:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code in this file!
+// Write your code in this file
+
+
+function scuberGreetingForFeet(feet) {
+  if (feet < 400) {return "This one is on me!"} 
+  if (feet > 2500) {return "No can do."} 
+  if (feet > 2000) {return "I will gladly take your thirty bucks."} 
+}
+
+function ternaryCheckCity(city) {
+if (city === "NYC") {return 'Ok, sounds good.'}
+if (city !== "NYC") {return 'No go.'}
+}
+
+function switchOnCharmFromTip(tip) {
+if (tip === "generous") {return 'Thank you so much.'}
+if (tip === "not as generous") {return 'Thank you.'}
+return 'Bye.'
+}
+
